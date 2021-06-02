891b7a84f66210d74bd498bb08be8cd6f0cea7ca
baraahshatnawi <>
2021-06-02 13:43:52 +0000

    added a naw file

---
+++ app.js
@@ -0,0 +1,12 @@
+alert("Welcome!Here is your chance to satisfy your CURIOSITY!")
+alert("You are in BARAAH's Website")
+
+let ready=prompt("ARE YOU READY?")
+
+alert (ready)
+if(ready == 'yes'){
+alert('Then here we GO!')  
+}
+else{
+alert('WHY ARE HERE!')
+}
